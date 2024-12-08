b83fd3292555a4bb2732584095c5b45b8fd77751
Tolynet <anatoly.slowetzky@gmail.com>
2024-12-08 14:13:50 -0300

    adding project

---
+++ src/test/java/com/Anatoly/Anatoly/AnatolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anatoly.Anatoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnatolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
