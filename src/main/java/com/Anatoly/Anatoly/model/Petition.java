df5854f54fdce9cf0f48c408d51349a750a6a4eb
Tolynet <anatoly.slowetzky@gmail.com>
2024-12-09 19:25:42 -0300

    Adding Petition Model

---
+++ src/main/java/com/Anatoly/Anatoly/model/Petition.java
@@ -0,0 +1,52 @@
+package com.Anatoly.Anatoly.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Petition {
+    private Long id;
+    private String title;
+    private String description;
+    private List<String> signatures = new ArrayList<>();
+
+    public Petition(Long id, String title, String description, List<String> signatures) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.signatures = signatures;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void addSignature(String signature) {
+        this.signatures.add(signature);
+    }
+
+    public List<String> getSignatures() {
+        return signatures;
+    }
+}
+
